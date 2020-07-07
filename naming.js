d2ea8fea1406060015daa810efc15c85e2755056
Nolan Barham <nolander91b@gmail.com>
2020-07-07 14:49:14 -0700

    added naming.js to workshop1

---
+++ naming.js
@@ -0,0 +1,22 @@
+var testVar1 = 5;
+var secondVarible = 2;
+var stringVarible = 'string';
+var secondString = 'foo';
+var lastString = 'kjfgklj'
+const constTest = 2;
+// this is 2//
+const secondConst = 4.6;
+const blahBlah = 3.14159;
+const yetMore = 342432523;
+const imDone = 123;
+
+console.log('testVar1', testVar1);
+console.log('secondVarible', secondVarible);
+console.log('stringVarible', stringVarible);
+console.log('secondStrong', secondString);
+console.log('lastString', lastString);
+console.log('constTest', constTest);
+console.log('secondConst', secondConst);
+console.log('blahBlah', blahBlah);
+console.log('yetMore', yetMore);
+console.log('imDone', imDone);
